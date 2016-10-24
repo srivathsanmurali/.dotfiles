350f1622512c6e1147e5bca4f0862f015defc4ea
Srivathsan Murali <srivathsan92@gmail.com>
2016-10-24 09:44:50 +0200

    batterys status with charging symbol

--- i3/.i3/scripts/battery.sh
+++ i3/.i3/scripts/battery.sh
@@ -2,8 +2,20 @@
 
 batPath0='/org/freedesktop/UPower/devices/battery_BAT0'
 batPath1='/org/freedesktop/UPower/devices/battery_BAT1'
+bat0Charging="$(acpitool -b | grep "#1" | awk -v cnt=0 '/Charging/ {++cnt} END {print cnt}')"
+bat1Charging="$(acpitool -b | grep "#2" | awk -v cnt=0 '/Charging/ {++cnt} END {print cnt}')"
 
-bat0="$(upower -i $batPath0 | grep 'percentage' | awk '{print $2}')"
-bat1="$(upower -i $batPath1 | grep 'percentage' | awk '{print $2}')"
+if [ $bat0Charging == 1 ]; then
+  bat0=" $(upower -i $batPath0 | grep 'percentage' | awk '{print $2}')"
+else
+  bat0="$(upower -i $batPath0 | grep 'percentage' | awk '{print $2}')"
+fi
 
-echo "[$bat0 , $bat1]"
+if [ $bat1Charging == 1 ]; then
+  bat1=" $(upower -i $batPath1 | grep 'percentage' | awk '{print $2}')"
+else
+  bat1="$(upower -i $batPath1 | grep 'percentage' | awk '{print $2}')"
+fi
+
+result="[ $bat0, $bat1 ]"
+echo $result 
