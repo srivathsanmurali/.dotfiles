e747d500ea93409a57ecdf052c82d02708e11d4e
Srivathsan Murali <srivathsan92@gmail.com>
2018-06-13 17:09:52 +0200

    added gpo and sorted aliases

--- zshCustom/scripts/aliases.zsh
+++ zshCustom/scripts/aliases.zsh
@@ -1,56 +1,57 @@
 ## Aliases
 # configs
+alias i3config='vi ~/.config/i3/config'
+alias nixconfig='sudoedit /etc/nixos/configuration.nix'
+alias vimconfig='vi ~/.vimrc'
 alias zshconfig='vi ~/.zshrc'
 alias zshrefresh='source ~/.zshrc'
-alias vimconfig='vi ~/.vimrc'
-alias i3config='vi ~/.i3/config'
-alias nixconfig='sudoedit /etc/nixos/configuration.nix'
 
 # git
 alias g='git'
 alias ga='git add'
 alias gap='git add -p'
+alias gb='git branch'
 alias gc='git commit'
-alias gs='git status'
+alias gchp='git cherry-pick'
+alias gcl='git clone'
+alias gco='git checkout'
 alias gd='git diff --color-words'
 alias gdc='git diff --color-words --cached'
-alias gco='git checkout'
-alias gchp='git cherry-pick'
-alias gp='git push'
-alias gb='git branch'
-alias gcl='git clone'
 alias gl='git l'
+alias gp='git push'
+alias gpo='git remote prune origin'
 alias gr='git gr'
+alias gs='git status'
 
 # misc
-alias emptyDir='rm -rf *'
-alias emacs='emacs -nw'
-alias vi='vim'
-alias v='vim'
-alias sv="sudoedit"
+#alias tmux='TERM=screen-256color-bce tmux'
 alias "cd.."="cd ../"
-alias cl=clear
-alias sagi="sudo apt-get install"
-alias sagr="sudo apt-get remove"
 alias asearch="apt-cache search"
-alias supdate="sudo apt-get update"
-alias grep="grep --color=auto"
-alias mk8="make -j4"
-alias mk="make"
-alias open="xdg-open"
+alias cl=clear
 alias copy="xclip -sel clip"
 alias copyPWD="pwd | xclip -sel clip"
-alias music="ncmpcpp"
-alias pp='python -mjson.tool'
-#alias tmux='TERM=screen-256color-bce tmux'
+alias emacs='emacs -nw'
+alias emptyDir='rm -rf *'
+alias grep="grep --color=auto"
 alias hgrep='history | grep '
 alias ls='ls --color=auto'
 alias matlab-cli='matlab -nodisplay -nosplash'
+alias mk8="make -j4"
 alias mk9="make -j9"
+alias mk="make"
+alias music="ncmpcpp"
+alias open="xdg-open"
+alias pp='python -mjson.tool'
+alias sagi="sudo apt-get install"
+alias sagr="sudo apt-get remove"
+alias supdate="sudo apt-get update"
+alias sv="sudoedit"
+alias v='vim'
+alias vi='vim'
 
 # Music
 alias music="ncmpcpp"
-alias toggleMusic="ncmpcpp toggle"
-alias playMusic="ncmpcpp play"
 alias pauseMusic="ncmpcpp pause"
+alias playMusic="ncmpcpp play"
 alias stopusic="ncmpcpp stop"
+alias toggleMusic="ncmpcpp toggle"
