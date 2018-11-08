dce1d7311a3432d98b6a3565551badea196853a4
Srivathsan Murali <srivathsan92@gmail.com>
2018-11-08 09:59:03 +0100

    updates to polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,11 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-DISPLAY1="$(xrandr -q | grep 'DVI' | cut -d ' ' -f1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar tbar -q&
-
-DISPLAY2="$(xrandr -q | grep 'HDMI' | cut -d ' ' -f1)"
-[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar tbar -q&
-
-echo $DISPLAY1
-echo $DISPLAY2
+polybar tbar &
