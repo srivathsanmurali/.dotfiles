32e1bd47b9dc1d07559767be3df0fa3c23b90993
Srivathsan Murali <srivathsan92@gmail.com>
2019-06-21 11:17:54 +0200

    reverting to go prompt

---
+++ bin_source/prompt.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CWD_PATH=$(echo ${PWD} | sed 's@'"${HOME}"'@~@')
+FOLDER=${CWD_PATH##*/}
+CWD_PATH=${CWD_PATH%"$FOLDER"}
+CWD_TOKENS=$(echo $CWD_PATH | tr "/" "\n")
+
+for token in $CWD_TOKENS
+do
+  if [ ${token::1} == "." ];
+  then echo -n "${token:0:2}/";
+  else echo -n "${token::1}/";
+  fi 
+done
+echo -n "$FOLDER"
