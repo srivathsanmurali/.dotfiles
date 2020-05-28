16123a051474e8a039508ab39d4a9d8ea3da94e1
Srivathsan Murali <sri@vathsan.com>
2020-05-28 07:46:37 +0200

    use host specific lock scripts

--- .local/bin/power_options.sh
+++ .local/bin/power_options.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 themes=$HOME/.config/rofi/monokai.rasi
-result=$(echo -e "lock\nlogout\nsuspend\npoweroff\nreboot" | rofi -dmenu -i -columns 5 -lines 1)
+result=$(echo -e "lock\nsuspend\npoweroff\nreboot" | rofi -dmenu -i -columns 5 -lines 1)
 
 case "$result" in
   "lock")
-    i3lock -c 000000
-    ;;
-  "logout")
-    i3-msg exit
+	lock
     ;;
   "suspend")
-    i3lock -c 000000;
-    systemctl suspend
+	lock suspend
     ;;
   "poweroff")
     systemctl poweroff
