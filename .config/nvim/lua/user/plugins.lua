7d9f59fd4533e1fc4acd460cef8f9e198a7fc9a2
Srivathsan Murali <sri@vathsan.com>
2022-10-05 22:35:31 +0800

    neovim/add nvim-tree

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -79,6 +79,10 @@ return packer.startup(function(use)
   -- Git signs
   use "lewis6991/gitsigns.nvim"
 
+  -- Nvim Tree
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
