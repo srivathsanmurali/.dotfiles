be90445d1c95860e1c2be3d4c50bd85e6b1c4a06
Srivathsan Murali <sri@vathsan.com>
2022-10-06 19:01:59 +0800

    neovim/add editorconfig

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "editorconfig/editorconfig-vim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp"
