6bad0a97d2620a8685bee967013ae03b32a97a47
Srivathsan Murali <sri@vathsan.com>
2022-12-14 21:14:19 +0800

    toggleterm

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -87,7 +87,10 @@ return packer.startup(function(use)
 
     -- Special
     use 'ixru/nvim-markdown'
-    
+
+    -- terminal
+    use "akinsho/toggleterm.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
