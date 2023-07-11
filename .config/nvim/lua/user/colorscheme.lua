5ca77b9af417eac32d71275282ac7cc117c496f2
Srivathsan Murali <sri@vathsan.com>
2023-07-11 15:19:16 +0800

    go dark

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
 -- https://gitlab.com/ali-lab/manifest-light
-local colorscheme = "manifest-light"
+local colorscheme = "base16-tokyo-night-dark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
