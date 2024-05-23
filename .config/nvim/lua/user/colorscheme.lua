ccfb28148a61c5041fb0fa2ffc9c006ae7e2a4d7
Srivathsan Murali <sri@vathsan.com>
2024-05-23 10:33:58 +0800

    updates

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,8 @@
 -- https://gitlab.com/ali-lab/manifest-light
 -- local colorscheme = "base16-tokyo-night-dark"
 -- local colorscheme = "base16-one-light"
-local colorscheme = "base16-catppuccin-macchiato"
+-- local colorscheme = "base16-catppuccin-macchiato"
+local colorscheme = "base16-catppuccin-latte"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
