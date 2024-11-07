4d091f453a687eca96234108f1128c3551c09965
Srivathsan Murali <sri@vathsan.com>
2024-11-07 16:27:06 +0800

    Add trouble.nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -90,4 +90,41 @@ return {
       }
     end,
   },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      -- {
+      --   '<leader>xx',
+      --   '<cmd>Trouble diagnostics toggle<cr>',
+      --   desc = 'Diagnostics (Trouble)',
+      -- },
+      -- {
+      --   '<leader>cs',
+      --   '<cmd>Trouble symbols toggle focus=false<cr>',
+      --   desc = 'Symbols (Trouble)',
+      -- },
+      -- {
+      --   '<leader>cl',
+      --   '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+      --   desc = 'LSP Definitions / references / ... (Trouble)',
+      -- },
+      -- {
+      --   '<leader>xL',
+      --   '<cmd>Trouble loclist toggle<cr>',
+      --   desc = 'Location List (Trouble)',
+      -- },
+      {
+        '<leader>tt',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = '[T]oggle [T]rouble Quickfix',
+      },
+      {
+        '<leader>td',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = '[T]oggle [D]iagnostics (Trouble)',
+      },
+    },
+  },
 }
